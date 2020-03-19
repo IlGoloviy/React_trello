f8711ab066ea5f40725491e96a4fe2d42b811f0d
Illia Holovii <goloviyillya@gmail.com>
2020-03-19 15:50:28 +0200

    Updates

---
+++ precache-manifest.60492c6eb3e1923bc41ff178678410ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8752755d8266b3db39d53e65c49b885c",
+    "url": "/React_trello/index.html"
+  },
+  {
+    "revision": "a7a4de3018a3aa060945",
+    "url": "/React_trello/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "51715ee533f621d32eba",
+    "url": "/React_trello/static/js/2.6cdc6124.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/React_trello/static/js/2.6cdc6124.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7a4de3018a3aa060945",
+    "url": "/React_trello/static/js/main.819805ce.chunk.js"
+  },
+  {
+    "revision": "f972b6fb10cdcbf35789",
+    "url": "/React_trello/static/js/runtime-main.53d289a1.js"
+  }
+]);
